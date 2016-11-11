530f22ca6e808ecf11b7a5fa016003f622b2b120
MrLarssonJr <jesper.ekvik@telia.com>
2016-11-11 16:32:27 +0100

    Clean up the mpu9150msg interface.
    Removed a lot of debugging checks.

--- Code/mpu9150msg.h
+++ Code/mpu9150msg.h
@@ -1,4 +1,29 @@
-int sendMessage(char slaveAddress, char slaveRegister, char dataByte);
+#pragma once
+
+/*
+ * Performs an Burst Write Sequence to a MPU9150 device.
+ *
+ * Returns 0 if successfull, -1 if not.
+ */
 int repeatedSendMessage(char slaveAddress, char firstSlaveRegister, char dataBytes[], int length);
-int receiveMessage(char slaveAddress, char slaveRegister);
+
+/*
+ * Send a message byte to a MPU9150 device.
+ *
+ * Returns 0 if successfull, -1 if not.
+ */
+int sendMessage(char slaveAddress, char slaveRegister, char dataByte);
+
+/*
+ * Performs a Burst Read Sequence to a MPU9150 device.
+ *
+ * Returns 1 if successfull, -1 otherwise.
+ */
 int repeatedReceiveMessage(char slaveAddress, char slaveRegister, int* receivedBytes, int times);
+
+/*
+ * Receive a message byte to a MPU9150 device.
+ *
+ * Returns 1 if successfull, -1 otherwise.
+ */
+int receiveMessage(char slaveAddress, char slaveRegister);
