30cbaa32af19fe66b2c7b0cf738e14c3973d76ac
MrLarssonJr <jesper.ekvik@telia.com>
2016-11-23 17:31:18 +0100

    Add missing register definitions.

--- Code/pwm.c
+++ Code/pwm.c
@@ -8,6 +8,67 @@
 
 #include <pic32mx.h>
 
+//#define OC1CON			PIC32_R (0x3000)
+#define OC1CONCLR		PIC32_R (0x3004)
+#define OC1CONSET		PIC32_R (0x3008)
+#define OC1CONINV		PIC32_R (0x300c)
+
+//#define OC4R			PIC32_R (0x3010)
+#define OC1RCLR			PIC32_R (0x3014)
+#define OC1RSET			PIC32_R (0x3018)
+#define OC1RINV			PIC32_R (0x301c)
+
+//#define OC1RS			PIC32_R (0x3020)
+#define OC1RSCLR		PIC32_R (0x3024)
+#define OC1RSSET		PIC32_R (0x3028)
+#define OC1RSINV		PIC32_R (0x302c)
+
+#define OC2CON			PIC32_R (0x3200)
+#define OC2CONCLR		PIC32_R (0x3204)
+#define OC2CONSET		PIC32_R (0x3208)
+#define OC2CONINV		PIC32_R (0x320c)
+
+#define OC2R			PIC32_R (0x3210)
+#define OC2RCLR			PIC32_R (0x3214)
+#define OC2RSET			PIC32_R (0x3218)
+#define OC2RINV			PIC32_R (0x321c)
+
+#define OC2RS			PIC32_R (0x3220)
+#define OC2RSCLR		PIC32_R (0x3224)
+#define OC2RSSET		PIC32_R (0x3228)
+#define OC2RSINV		PIC32_R (0x322c)
+
+
+#define OC3CON			PIC32_R (0x3400)
+#define OC3CONCLR		PIC32_R (0x3404)
+#define OC3CONSET		PIC32_R (0x3408)
+#define OC3CONINV		PIC32_R (0x340c)
+
+#define OC3R			PIC32_R (0x3410)
+#define OC3RCLR			PIC32_R (0x3414)
+#define OC3RSET			PIC32_R (0x3418)
+#define OC3RINV			PIC32_R (0x341c)
+
+#define OC3RS			PIC32_R (0x3420)
+#define OC3RSCLR		PIC32_R (0x3424)
+#define OC3RSSET		PIC32_R (0x3428)
+#define OC3RSINV		PIC32_R (0x342c)
+
+//#define OC4CON			PIC32_R (0x3600)
+#define OC4CONCLR		PIC32_R (0x3604)
+#define OC4CONSET		PIC32_R (0x3608)
+#define OC4CONINV		PIC32_R (0x360c)
+
+//#define OC4R			PIC32_R (0x3610)
+#define OC4RCLR			PIC32_R (0x3614)
+#define OC4RSET			PIC32_R (0x3618)
+#define OC4RINV			PIC32_R (0x361c)
+
+//#define OC4RS			PIC32_R (0x3620)
+#define OC4RSCLR		PIC32_R (0x3624)
+#define OC4RSSET		PIC32_R (0x3628)
+#define OC4RSINV		PIC32_R (0x362c)
+
 const int period = 0x00C7;
 
 
@@ -19,9 +80,9 @@ void pwm_initTimer() {
 	// Configure Timer2 interrupt. Note that in PWM mode, the
 	// corresponding source timer interrupt flag is asserted.
 	// OC interrupt is not generated in PWM mode.
-	IFS0CLR = 0x00000100; // Clear the T2 interrupt flag
-	IEC0SET = 0x00000100; // Enable T2 interrupt
-	IPC2SET = 0x0000001C; // Set T2 interrupt priority to 7
+	IFSCLR(0) = 0x00000100; // Clear the T2 interrupt flag
+	IECSET(0) = 0x00000100; // Enable T2 interrupt
+	IPCSET(2) = 0x0000001C; // Set T2 interrupt priority to 7
 	T2CONSET = 0x8000; // Enable Timer2
 }
 
