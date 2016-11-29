229e15fb92ce17196f08575d89afcece7bfe0a52
MrLarssonJr <jesper.ekvik@telia.com>
2016-11-29 11:46:23 +0100

    Add "log" messages for the startup procedure.

--- Code/quad.c
+++ Code/quad.c
@@ -13,22 +13,29 @@ void quad_init(void) {
 
 	// Init input
 	input_init();
+	display_string(0, "Initialized input");
 
 	//Init sensors
 	mpu9150interface_setup();
 	mpu9150interface_awaken();
+	display_string(0, "Initialized sensors");
 
 	// Init ESCs depending on switch config
 	switch (input_getInput(START_MODE_SWITCH)) {
-		case 1: {
+		case 0: {
+			display_string(0, "Doing normal ESC start");
 			esc_init(NORMAL_START);
 			break;
 		}
-		case 0: {
+		case 1: {
+			display_string(0, "Setting throttle range on ESC start");
 			esc_init(SET_THROTTLE_RANGE_START);
 			break;
 		}
 	}
+	display_string(0, "Initialized ESCs");
+
+	display_string(0, "Initialization done");
 }
 
 void quad_loop(void) {
