7177eb3f1b38ce85b8342d16de9713f174317d76
Zalodu <peter.kjellen@gmail.com>
2016-12-07 21:32:34 +0100

    Swap rear and left for hardware change

--- Code/hardwareConfig.h
+++ Code/hardwareConfig.h
@@ -8,6 +8,6 @@ RIGHT: counter-clockwise
 */
 
 #define MOTOR_FRONT_MODULE 1
-#define MOTOR_REAR_MODULE 3
-#define MOTOR_LEFT_MODULE 4
+#define MOTOR_REAR_MODULE 4
+#define MOTOR_LEFT_MODULE 3
 #define MOTOR_RIGHT_MODULE 2
