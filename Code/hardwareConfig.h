32b61944461bbbef432a5610bc3703b37ef1a015
Zalodu <peter.kjellen@gmail.com>
2016-12-07 20:32:25 +0100

    Add information about rotational directions

--- Code/hardwareConfig.h
+++ Code/hardwareConfig.h
@@ -1,5 +1,12 @@
 #pragma once
 
+/*
+FRONT: clockwise
+BACK: clockwise
+LEFT: counter-clockwise
+RIGHT: counter-clockwise
+*/
+
 #define MOTOR_FRONT_MODULE 1
 #define MOTOR_REAR_MODULE 3
 #define MOTOR_LEFT_MODULE 4
